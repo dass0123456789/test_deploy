8ab92e64ce86166afd90ceb7f4138fc7348e5068
bas <dass0123456789@gamil.com>
2026-06-16 19:08:39 +0700

    second

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,11 +1,10 @@
 import express from "express"
 import {register,getAllUser} from "../controllers/auth.controller.js"
+import uploads from "../middleware/upload.js"
 const route =express.Router()
 
 route.get('/listalluer',getAllUser)
 route.post('/register',register)
-
-
-
+route.post('/uploads',uploads,(req,res,next)=>{res.json({msg:"uploads success"})})
 
 export default route
